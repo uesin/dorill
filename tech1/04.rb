76f4fe1809f56aeb6e68c5b83390b9b2f44ef387
uesin <kazukihana87@gmail.com>
2020-01-29 23:59:43 +0900

    1月29日分

---
+++ tech1/04.rb
@@ -0,0 +1,28 @@
+#下記の（１）を変換して（２）にするためのコードを記述してください。
+#（１） "Today" " is" " sunny"
+#（２） "Today is sunny"
+
+puts "Today" + " is" + " suuny"
+
+#下記の（１）を変換して（２）にするためのコードを記述してください。
+#（１） "Ruby, Rails, JavaScript"
+#（２） ["Ruby", " Rails", " JavaScript"]
+
+puts "Ruby, Rails, JavaScript".split(",")
+
+
+#下記の（１）を変換して（２）にするためのコードを記述してください。
+#（１） "rails"
+#（２） "Rails"
+
+puts "rails".capitalize
+
+#下記の（１）を変換して（２）にするためのコードを記述してください。
+#（１） "abcde"
+#（２） "edcba"
+puts "abcde".reverse
+#下記の（１）を変換して（２）にするためのコードを記述してください。
+#（１） "Hello, world"
+#（２） "HELLO, WORLD"
+
+puts "Hello, world".upcase
